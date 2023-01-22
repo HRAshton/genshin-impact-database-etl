f160083115ca17c44fe0020ce08dee85ebf8d08b
HRAshton <12210721+HRAshton@users.noreply.github.com>
2023-01-22 16:23:52 +0400

    Add fetch rate limit

--- src/GasAdapters/FetchingService.js
+++ src/GasAdapters/FetchingService.js
@@ -1,6 +1,15 @@
 class FetchingService {
+  constructor() {
+    this.callsTimeoutMs = 500;
+    this.lastRequestTimestamp = new Date().getTime();
+  }
+
   async fetchAsync(url) {
-    Utilities.sleep(500);
+    const timestamp = new Date().getTime();
+    const timeToWait = this.callsTimeoutMs - (timestamp - this.lastRequestTimestamp);
+    Utilities.sleep(Math.max(timeToWait, 0));
+    this.lastRequestTimestamp = timestamp;
+
     return new Promise(resolve => resolve(UrlFetchApp.fetch(url).getContentText()));
   }
 }
